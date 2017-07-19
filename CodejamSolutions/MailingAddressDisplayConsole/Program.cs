45b1298611cd1c9ce1c707270e19580bdd223391
divastavisca <dagarwal@tavisca.com>
2017-07-19 18:46:32 +0530

    final changes to assignment 1

--- CodejamSolutions/MailingAddressDisplayConsole/Program.cs
+++ CodejamSolutions/MailingAddressDisplayConsole/Program.cs
@@ -12,61 +12,12 @@ namespace MailingAddressDisplayConsole
         /*Assignment Q1: Write a program to display user’s complete mailing address. 
          Accept user’s name, city, street, pin and house no. and store it in a variable and display it.*/
 
-        //This program validates all input provided
+        //This program validates all inputs provided
 
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            User newUser = new User();
-            InputFromConsole newScanner = new InputFromConsole();          //Custom console object
-            ValidateCredentials newValidator = new ValidateCredentials();  //Custom validation object
-            bool brk = true;
-            string firstName="",lastName="",street="",pinCode="",city="";
-            int pnCode=0;
-            while (brk)                        //Get first name from console until gets a valid input
-            {
-                firstName = newScanner.Read("Enter your first name");
-                brk=!newValidator.IsValidStringName(firstName);
-                if (brk)
-                    Console.WriteLine("Invalid");
-            }
-            newUser.FirstName = firstName;
-            brk = true;
-            while (brk)                         //Get last name from console until gets a valid input
-            {
-                lastName = newScanner.Read("Enter your last name");
-                brk = !newValidator.IsValidStringName(lastName);
-                if (brk)
-                    Console.WriteLine("Invalid");
-            }
-            newUser.LastName = lastName;
-            brk = true;
-            newUser.mailingAddress.SetHouseNo(newScanner.Read("Enter your house number"));   //Get House no
-            while(brk)                                   //Get street from console until gets a valid input
-            {
-                street = newScanner.Read("Enter street");
-                brk = !newValidator.IsValidStringName(street);
-                if (brk)
-                    Console.WriteLine("Invalid");
-            }
-            newUser.mailingAddress.SetStreet(street);
-            brk = true;
-            while (brk)                                   //Get city from console until gets a valid input
-            {
-                city = newScanner.Read("Enter City");
-                brk = !newValidator.IsValidStringName(city);
-                if (brk)
-                    Console.WriteLine("Invalid");
-            }
-            newUser.mailingAddress.SetCity(city);
-            brk = true;
-            while (brk)                                  //Get pincode from console until gets a valid input
-            {
-                pinCode = newScanner.Read("Enter Pincode");
-                brk = !newValidator.TryParsePinCode(pinCode,out pnCode);
-                if (brk)
-                    Console.WriteLine("Invalid");
-            }
-            newUser.mailingAddress.SetPinCode(pnCode);
+            ReturnValidInput ValidInput = new ReturnValidInput();
+            User newUser = ValidInput.GetValidInput();
             Console.WriteLine("\nUser's Address\n--------------\n"+newUser.mailingAddress);
             Console.ReadKey();
         }
--- CodejamSolutions/MailingAddressDisplayConsole/ReturnValidInput.cs
+++ CodejamSolutions/MailingAddressDisplayConsole/ReturnValidInput.cs
@@ -11,5 +11,43 @@ namespace MailingAddressDisplayConsole
     {
         InputFromConsole newScanner = new InputFromConsole();          //Custom console object
         ValidateCredentials newValidator = new ValidateCredentials();  //Custom validation object
+
+        public User GetValidInput()
+        {
+            User newUser = new User();
+            bool brk = true;
+            string pinCode = "";
+            int pnCode = 0;
+            newUser.FirstName = GetInputString("Enter your first name");  //set firstname
+            newUser.LastName = GetInputString("Enter your last name");    //set lastname
+            brk = true;
+            newUser.mailingAddress.SetHouseNo(newScanner.Read("Enter your house number"));   //Get House no
+            newUser.mailingAddress.SetStreet(GetInputString("Enter street"));   //set street
+            newUser.mailingAddress.SetCity(GetInputString("Enter City"));     //set city
+            brk = true;
+            while (brk)                                  //Get pincode from console until gets a valid input
+            {
+                pinCode = newScanner.Read("Enter Pincode");
+                brk = !newValidator.TryParsePinCode(pinCode, out pnCode);
+                if (brk)
+                    Console.WriteLine("Invalid");
+            }
+            newUser.mailingAddress.SetPinCode(pnCode);   //set pincode
+            return newUser;
+        }
+
+        public string GetInputString(string query)
+        {
+            bool brk = true;
+            string InputString = "";
+            while (brk)                        //Get first name from console until gets a valid input
+            {
+                InputString = newScanner.Read(query);
+                brk = !newValidator.IsValidStringName(InputString);
+                if (brk)
+                    Console.WriteLine("Invalid");
+            }
+            return InputString;
+        }
     }
 }
