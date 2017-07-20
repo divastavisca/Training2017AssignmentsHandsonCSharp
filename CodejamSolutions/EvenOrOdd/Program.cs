2ade1cda046b42f553bc1625b22e8974b9afecaf
divastavisca <dagarwal@tavisca.com>
2017-07-20 11:53:29 +0530

    refactoring done to even odd program

--- CodejamSolutions/EvenOrOdd/Program.cs
+++ CodejamSolutions/EvenOrOdd/Program.cs
@@ -3,32 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SupportLibrary;
 
 namespace EvenOrOdd
 {
     public class Program
     {
-      //Assignment Q3: Write a program to check whether input number is even or odd.
-        
+        //Assignment Q3: Write a program to check whether input number is even or odd.
+
+        static InputFromConsole reader = new InputFromConsole();
         public static void Main(string[] args)
         {
-            Console.WriteLine("Enter a number");
-            bool brk = true;
-            long inputNumber;
-            string result="";
-            while(brk)
-            {
-                if(Int64.TryParse(Console.ReadLine(),out inputNumber)&&inputNumber!=0)   //Check for valid input
-                {
-                    brk = false;
-                    result = CheckForEvenOrOdd(inputNumber);   //Final result
-                }
-                else
-                {
-                    Console.WriteLine("Invalid Input");
-                }
-            }
-            Console.WriteLine("Entered number is " + result);   //Output
+            long inputNumber=reader.ReadInt("Enter a number");
+            Console.WriteLine("Entered number is " + CheckForEvenOrOdd(inputNumber));   //Output
             Console.ReadKey();
         }
 
